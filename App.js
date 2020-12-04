4ba50f5a4cdcb505c7eba39174d1d88cca3d718a
Siddhartha Bhagwan <sbhagwan@tesla.com>
2020-12-04 15:04:39 -0800

    Footer added, verses 15 - 20

--- App.js
+++ App.js
@@ -1,38 +1,19 @@
 import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, TouchableOpacity, Text } from 'react-native';
 import Header from './src/components/Header.js';
+// import Footer from './src/components/Footer.js';
 import Verse from './src/components/Verse';
 import { StatusBar } from 'expo-status-bar';
 import verses from './assets/verses';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
-import MenuDrawer from 'react-native-side-drawer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faListUl, faThLarge } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from './src/components/Sidebar';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faListUl, faThLarge } from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
-	const [verseNo, setVerseNo] = useState(1);
+	const [verseNo, setVerseNo] = useState(2);
 	const [drawer, setDrawer] = useState(false);
-
-	const toggleDrawer = () => setDrawer(!drawer);
-
-	const drawerContent = () => {
-		const v = Array.from({ length: 10 }, (_, i) => i + 1);
-
-		const verses = v.map((v) => (
-			<Text
-				onPress={() => {
-					setVerseNo(v);
-					toggleDrawer();
-				}}
-				style={styles.verseMenu}
-			>
-				{v}
-			</Text>
-		));
-
-		return <TouchableOpacity style={styles.menu}>{verses}</TouchableOpacity>;
-	};
 
 	const [isLoaded] = useFonts({
 		'meta-normal': require('./assets/fonts/meta-normal.ttf'),
@@ -43,16 +24,21 @@ export default function App() {
 	} else {
 		return (
 			<SafeAreaView style={styles.container}>
-				<MenuDrawer
-					open={drawer}
-					drawerContent={drawerContent()}
-					drawerPercentage={15}
-					animationTime={250}
-					overlay={true}
-					opacity={0.4}
-				></MenuDrawer>
-				<Header key="Header" toggleDrawer={toggleDrawer} />
+				<Sidebar
+					key="sidebar"
+					drawer={drawer}
+					verseNo={verseNo}
+					setDrawer={setDrawer}
+					setVerseNo={setVerseNo}
+				/>
+				<Header
+					key="header"
+					drawer={drawer}
+					setDrawer={setDrawer}
+					setVerseNo={setVerseNo}
+				/>
 				<Verse key="chapters" verseNo={verseNo} text={verses[verseNo]} />
+				{/* <Footer setVerseNo={setVerseNo} verseNo={verseNo} /> */}
 				<StatusBar style="auto" />
 			</SafeAreaView>
 		);
@@ -63,18 +49,5 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: 'white',
-	},
-	menu: {
-		flex: 1,
-		padding: 10,
-		backgroundColor: 'white',
-		border: '1px solid #dfdfdf',
-	},
-	verseMenu: {
-		fontSize: 18,
-		padding: 5,
-		borderBottom: '1px solid #dddddd',
-		textAlign: 'center',
-		fontFamily: 'meta-normal',
 	},
 });
--- assets/verses.js
+++ assets/verses.js
@@ -43,15 +43,71 @@ const verses = {
 		'In fear that it may be frayed, or stained with dust he keeps himself from the world, and is afraid even to move. \n \n' +
 		'Mother, it is no gain, thy bondage of finery, if it keep one shut off from the healthful dust of the earth, if it rob one of the right of entrance to the great fair of common human life. \n \n',
 	9:
-		'O FOOL, to try to carry thyself upon thy own shoulders! O beggar, to come to beg at thyown door! \n \n' +
+		'O fool, to try to carry thyself upon thy own shoulders! O beggar, to come to beg at thyown door! \n \n' +
 		'Leave all thy burdens on his hands who can bear all, and never look behind in regret. \n \n' +
 		'Thy desire at once puts out the light from the lamp it touches with its breath. \n \n' +
 		'It is unholy ⎯ take not thy gifts through its unclean hands. Accept only what is offered by sacred love. \n \n',
 	10:
-		'HERE is thy footstool and there rest thy feet where live the poorest, and lowliest, and lost.\n \n' +
+		'Here is thy footstool and there rest thy feet where live the poorest, and lowliest, and lost.\n \n' +
 		'When I try to bow to thee, my obeisance cannot reach down to the depth where thy feet rest among the poorest, and lowliest, and lost.\n \n' +
 		'Pride can never approach to where thou walkest in the clothes of the humble among the poorest, and lowliest, and lost.\n \n' +
 		'My heart can never find its way to where thou keepest company with the companionless among the poorest, the lowliest, and the lost.\n \n',
+	11:
+		'Leave this chanting and singing and telling of beads! Whom dost thou worship in this lonely dark corner of a temple with doors all shut? Open thine eyes and see thy God is not before thee! \n \n' +
+		'He is there where the tiller is tilling the hard ground and where the path-maker is breaking stones. He is with them in sun and in shower, and his garment is covered with dust. Put off thy holy mantle and even like him come down on the dusty soil! \n \n' +
+		'Deliverance? Where is this deliverance to be found? Our master himself has joyfully taken upon him the bonds of creation; he is bound with us all for ever. \n \n' +
+		'Come out of thy meditations and leave aside thy flowers and incense! \n \n' +
+		'What harm is there if thy clothes become tattered and stained? Meet him and stand by him in toil and in sweat of thy brow. \n \n',
+	12:
+		'The time that my journey takes is long and the way of it long.\n \n' +
+		'I came out on the chariot of the first gleam of light, and pursued my voyage through the wildernesses of worlds leaving my track on many a star and planet.\n \n' +
+		'It is the most distant course that comes nearest to thyself, and that training is the most intricate which leads to the utter simplicity of a tune.\n \n' +
+		'The traveller has to knock at every alien door to come to his own, and one has to wander through all the outer worlds to reach the innermost shrine at the end.\n \n' +
+		'My eyes strayed far and wide before I shut them and said "Here art thou!"\n \n' +
+		'The question and the cry "Oh, where?" melt into tears of a thousand streams and deluge the world with the flood of the assurance "I am!"\n \n',
+	13:
+		'The song that I came to sing remains unsung to this day.\n \n ' +
+		'I have spent my days in stringing and in unstringing my instrument.\n \n ' +
+		'The time has not come true, the words have not been rightly set; only there is the agony of wishing in my heart.\n \n ' +
+		'The blossom has not opened; only the wind is sighing by.\n \n ' +
+		'I have not seen his face, nor have I listened to his voice; only I have heard his gentle footsteps from the road before my house.\n \n ' +
+		'The livelong1 day has passed in spreading his seat on the floor; but the lamp has not been lit and I cannot ask him into my house.\n \n ' +
+		'I live in the hope of meeting with him; but this meeting is not yet.\n \n ',
+	14:
+		'My desires are many and my cry is pitiful, but ever didst thou save me by hard refusals;\n\n' +
+		'and this strong mercy has been wrought into my life through and through.\n\n' +
+		'Day by day thou art making me worthy of the simple, great gifts that thou gavest to me unasked ⎯ this sky and the light, this body and the life and the mind ⎯ saving me from perils of overmuch desire.\n\n' +
+		'There are times when I languidly linger and times when I awaken and hurry in search of my goal; but cruelly thou hidest thyself from before me.\n\n' +
+		'Day by day thou art making me worthy of thy full acceptance by refusing me ever and anon1, saving me from perils of weak, uncertain desire.\n\n',
+	15:
+		'I AM here to sing thee songs. In this hall of thine I have a corner seat.\n\n' +
+		'In thy world I have no work to do; my useless life can only break out in tunes without a purpose.\n\n' +
+		'When the hour strikes for thy silent worship at dark temple of midnight, command me, my master, to stand before thee to sing.\n\n' +
+		'When in the morning air the golden harp is tuned, honour me, commanding my presence.\n\n',
+	16:
+		"I HAVE had my invitation to this world's festival, and thus my life has been blessed. My eyes have seen and my ears have heard. \n\n" +
+		'It was my part at this feast to play upon my instrument, and I have done all I could.\n\n' +
+		'Now, I ask, has the time come at last when I may go in and see thy face and offer thee my silent salutation?\n\n' +
+		'I AM only waiting for love to give myself up at last into his hands. That is why it is so late and why I have been guilty of such omissions.\n\n',
+	17:
+		'They come with their laws and their codes to bind me fast; but I evade them ever, for I am only waiting for love to give myself up at last into his hands.\n\n' +
+		'People blame me and call me heedless; I doubt not they are right in their blame.\n\n' +
+		'The market day is over and work is all done for the busy. Those who came to call me in vain have gone back in anger. I am only waiting for love to give myself up at last into his hands.\n\n',
+	18:
+		'Clouds heap upon clouds and it darkens. Ah, love, why dost thou let me wait outside at the door all alone?\n\n' +
+		'In the busy moments of the noontide work I am with the crowd, but on this dark lonely day it is only for thee that I hope.\n\n' +
+		'If thou showest me not thy face, if thou leavest me wholly aside, I know not how I am to pass these long, rainy hours.\n\n' +
+		'I keep gazing on the far away gloom of the sky, and my heart wanders wailing with the restless wind.\n\n',
+	19:
+		'If thou speakest not I will fill my heart with thy silence and endure it. I will keep still and\n\n' +
+		'wait like the night with starry vigil and its head bent low with patience.\n\n' +
+		'The morning will surely come, the darkness will vanish, and thy voice pour down in golden streams breaking through the sky.\n\n' +
+		"Then thy words will take wing in songs from every one of my birds' nests, and thy melodies will break forth in flowers in all my forest groves.",
+	20:
+		'On the day when the lotus bloomed, alas, my mind was straying, and I knew it not. My basket was empty and the flower remained unheeded. \n\n' +
+		'Only now and again a sadness fell upon me, and I started up from my dream and felt a sweet trace of a strange fragrance in the south wind. \n\n' +
+		'That vague sweetness made my heart ache with longing and it seemed to me that it was the eager breath of the summer seeking for its completion. \n\n' +
+		'I knew not then that it was so near, that it was mine, and that this perfect sweetness had blossomed in the depth of my own heart. \n\n',
 };
 
 export default verses;
