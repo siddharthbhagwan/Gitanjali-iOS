bb03bd7636ebfca5236fe1ad99d7b4b50e21d927
Siddhartha Bhagwan <sbhagwan@tesla.com>
2020-12-03 21:35:57 -0800

    can read poem from text file

---
+++ assets/chapters.js
@@ -0,0 +1,16 @@
+const chapters = {
+  1:
+    'Thou hast made me endless, such is thy pleasure. This frail vessel thou emptiest again and again, and fillest it ever with fresh life. \n \n' +
+    'This little flute of a reed thou hast carried over hills and dales, and hast breathed through it melodies eternally new. \n \n' +
+    'At the immortal touch of thy hands my little heart loses its limits in joy and gives birth to utterance ineffable. \n \n' +
+    'Thy infinite gifts come to me only on these very small hands of mine. Ages pass, and still thou pourest, and still there is room to fill.\n \n',
+  2:
+    'When thou commandest me to sing it seems that my heart would break with pride; and \n \n' +
+    'I look to thy face, and tears come to my eyes. \n \n' +
+    'All that is harsh and dissonant in my life melts into one sweet harmony ⎯ and my adoration spreads wings like a glad bird on its flight across the sea. \n \n' +
+    'I know thou takest pleasure in my singing. I know that only as a singer I come before thy presence. \n \n' +
+    'I touch by the edge of the far spreading wing of my song thy feet which I could never aspire to reach. \n \n' +
+    'Drunk with the joy of singing I forget myself and call thee friend who art my lord. \n \n',
+};
+
+export default chapters;
