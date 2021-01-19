fe874d951e39394393589c93ec01823f31c120cc
Siddhartha Bhagwan <sbhagwan@tesla.com>
2021-01-18 19:39:55 -0800

    Splash screen timed

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import HomeScreen from './src/components/HomeScreen';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+	const [splash, setSplash] = useState(true);
 	const [isLoaded] = useFonts({
 		'meta-normal': require('./assets/fonts/meta-normal.ttf'),
 	});
 
-	if (!isLoaded) return <AppLoading />;
+	SplashScreen.preventAutoHideAsync();
+
+	useEffect(
+		() =>
+			setTimeout(() => {
+				SplashScreen.hideAsync();
+				setSplash(false);
+			}, 1500),
+		[]
+	);
+
+	if (!isLoaded || splash) return <AppLoading />;
 	else return <HomeScreen />;
 }
